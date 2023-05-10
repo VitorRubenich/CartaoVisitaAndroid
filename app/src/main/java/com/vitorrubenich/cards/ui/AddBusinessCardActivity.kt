9179f0d29d729d2c8352a785f2f5fd847d872591
VitorRubenich <rubenich.vitor@gmail.com>
2023-05-10 08:25:26 -0300

    Cartão visita funcional

--- app/src/main/java/com/vitorrubenich/cards/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/com/vitorrubenich/cards/ui/AddBusinessCardActivity.kt
@@ -1,13 +1,20 @@
 package com.vitorrubenich.cards.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.vitorrubenich.cards.App
+import com.vitorrubenich.cards.R
+import com.vitorrubenich.cards.data.entities.BusinessCard
 import com.vitorrubenich.cards.databinding.ActivityAddBusinessCardBinding
 
 class AddBusinessCardActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +27,16 @@ class AddBusinessCardActivity : AppCompatActivity() {
             finish()
         }
         binding.fabConfirmar.setOnClickListener {
-            Snackbar.make(binding.root, "Confirmado", Snackbar.LENGTH_SHORT).show()
+            val businessCard = BusinessCard(
+            nome = binding.tilNome.editText?.text.toString(),
+            empresa = binding.tilEmpresa.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                fundoPersonalizado = binding.tilCor.editText?.text.toString()
+                )
+            mainViewModel.insert(businessCard)
+            Snackbar.make(binding.root, R.string.label_success,Snackbar.LENGTH_LONG).show()
+            finish()
         }
     }
 }
--- app/src/main/java/com/vitorrubenich/cards/ui/MainActivity.kt
+++ app/src/main/java/com/vitorrubenich/cards/ui/MainActivity.kt
@@ -2,16 +2,26 @@ package com.vitorrubenich.cards.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.vitorrubenich.cards.App
 import com.vitorrubenich.cards.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
+    private val mainViewModel: MainViewModel by viewModels{
+        MainViewModelFactory((application as App).repository)
+    }
+    private val adapter by lazy { BusinessCardAdapter() }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.listCards.adapter = adapter
+        getAllBusinessCard()
         insertListeners()
     }
 
@@ -21,6 +31,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java )
             startActivity(intent)
         }
-
+    }
+    private fun getAllBusinessCard(){
+        mainViewModel.getAll().observe(this) {
+            businessCards -> adapter.submitList(businessCards)
+        }
     }
 }
