b6fb33d94274bc9e83a30f31b0c7b4426dfe0ef8
VitorRubenich <rubenich.vitor@gmail.com>
2023-05-12 14:04:27 -0300

    Testes de inclusão de BottomAppBar

--- app/src/main/java/com/vitorrubenich/cards/ui/MainActivity.kt
+++ app/src/main/java/com/vitorrubenich/cards/ui/MainActivity.kt
@@ -2,9 +2,12 @@ package com.vitorrubenich.cards.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.vitorrubenich.cards.App
+import com.vitorrubenich.cards.R
 import com.vitorrubenich.cards.databinding.ActivityMainBinding
 import com.vitorrubenich.cards.util.Image
 
@@ -34,6 +37,30 @@ class MainActivity : AppCompatActivity() {
         }
         adapter.listenerShare = { card ->
             Image.share(this@MainActivity, card)
+        }
+        binding.bottomAppBar.setOnMenuItemClickListener{
+            Snackbar.make(binding.root,"$it", Snackbar.LENGTH_LONG).show()
+            true
+        }
+        binding.bottomAppBar.setNavigationOnClickListener{menuItem ->
+            Log.e("LISTENER", "insertListeners: ${menuItem.id}")
+            when (menuItem.id) {
+                R.id.door -> {
+                    Snackbar.make(binding.root, resources.getString(R.string.door), Snackbar.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.rotation -> {
+                    Snackbar.make(binding.root, resources.getString(R.string.rotation), Snackbar.LENGTH_SHORT).show()
+
+                    true
+                }
+                R.id.dashboard -> {
+                    Snackbar.make(binding.root, resources.getString(R.string.dashboard), Snackbar.LENGTH_SHORT).show()
+
+                    true
+                }
+                else -> false
+            }
         }
     }
     private fun getAllBusinessCard(){
